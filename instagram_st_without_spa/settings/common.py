cafb6c44a69ba48fca3d9cfb269a5618a1295b19
joje1283 <joje1283@gmail.com>
2020-11-03 23:28:46 +0900

    챕터 7) 비SPA 방식으로 장고 Forms/Views를 적극 활용한 인스타그램 St 만들기 04 커스텀 유저 지정 및 회원가입 구현

--- instagram_st_without_spa/settings/common.py
+++ instagram_st_without_spa/settings/common.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third Apps
     'debug_toolbar',
+    'bootstrap4',
     # Locals Apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -88,6 +90,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- instagram_st_without_spa/urls.py
+++ instagram_st_without_spa/urls.py
@@ -1,13 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     # re_path('', TemplateView.as_view(template_name='root.html'), name='root'),  # 모든 경우에 매칭되는 URL
-    path('', TemplateView.as_view(template_name='root.html'), name='root')
+    path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
 ]
 
 if settings.DEBUG:
