5268c26dfffb45400c0d29d1c281f2b92f269232
joje1283 <joje1283@gmail.com>
2020-11-06 23:11:40 +0900

    챕터 7) 비SPA 방식으로 장고 Forms/Views를 적극 활용한 인스타그램 St 만들기 07 로그인-로그아웃 구현 그리고 회원 가입과 동시에 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', views.login, name='login'),  # /accounts/login/ => settings.LOGIN_URL
+    path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
