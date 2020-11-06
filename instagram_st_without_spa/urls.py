8b0edb1c3719e3259fdac07018d80e891fd0b694
joje1283 <joje1283@gmail.com>
2020-11-06 23:24:56 +0900

    챕터 7) 비SPA 방식으로 장고 Forms/Views를 적극 활용한 인스타그램 St 만들기 08 django-pydenticon을 활용하여 프로필 디폴트 이미지 구현

--- instagram_st_without_spa/urls.py
+++ instagram_st_without_spa/urls.py
@@ -5,11 +5,12 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from django_pydenticon.views import image as pydenticon_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('identicon/image/<path:data>/', pydenticon_image, name='pydenticon_image'),
     # re_path('', TemplateView.as_view(template_name='root.html'), name='root'),  # 모든 경우에 매칭되는 URL
     path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
 ]
