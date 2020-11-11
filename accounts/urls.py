b6f1955db2688eaa833b3afc9178964fca0374d5
joje1283 <joje1283@gmail.com>
2020-11-11 23:46:06 +0900

    챕터 7) 비SPA 방식으로 장고 Forms/Views를 적극 활용한 인스타그램 St 만들기 10 auth앱의 PasswordChangeForm 커스텀을 통한 암호 수정 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from django.views.generic import TemplateView
 from . import views
+
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.login, name='login'),  # /accounts/login/ => settings.LOGIN_URL
     path('logout/', views.logout, name='logout'),
+    path('password_change/', views.password_change, name='password_change'),
     path('signup/', views.signup, name='signup'),
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
